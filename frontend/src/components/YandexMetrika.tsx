932aa3f806d4a2fea507fa31917b7663555b7efe
Eglor <leontevegor57@gmail.com>
2026-04-08 13:37:42 +0500

    Update YandexMetrika.tsx

--- frontend/src/components/YandexMetrika.tsx
+++ frontend/src/components/YandexMetrika.tsx
@@ -28,13 +28,17 @@ export default function YandexMetrika() {
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=${COUNTER_ID}', 'ym');
 
           ym(${COUNTER_ID}, 'init', {
+            ssr: true,
+            webvisor: true,
             clickmap: true,
-            trackLinks: true,
+            ecommerce: "dataLayer",
+            referrer: document.referrer,
+            url: location.href,
             accurateTrackBounce: true,
-            webvisor: true
+            trackLinks: true
           });
         `}
       </Script>
